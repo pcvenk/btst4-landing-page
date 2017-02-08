b9a365266fa7eaf52eab67931cb549c29153c610
Primoz Cvenkelj <primoz.cvenkelj@gmail.com>
2017-02-08 11:40:04 +0100

    Scroll animation to different sections on the page

--- js/main.js
+++ js/main.js
@@ -1,3 +1,12 @@
 $('#why .fa').hover(function(){
     $(this).toggleClass('animated pulse');
 });
+
+//scrolling to a div
+$(".scroll-to").on("click", function(e) {
+    e.preventDefault();
+    $("body, html").animate({
+        scrollTop: $($(this).attr('href')).offset().top
+    }, 1500);
+
+});
